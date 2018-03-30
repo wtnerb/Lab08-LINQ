bd3ecc573f0c4a9734298e014c881c646ac13197
wtnerb <wtnerb@gmail.com>
2018-03-30 10:20:33 -0700

    code complete

---
+++ lab08-brent/lab08-brent/JsonClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab08_brent
+{
+    class JsonClass
+    {
+
+        public class Rootobject
+        {
+            public string type { get; set; }
+            public Feature[] features { get; set; }
+        }
+
+        public class Feature
+        {
+            public string type { get; set; }
+            public Geometry geometry { get; set; }
+            public Properties properties { get; set; }
+        }
+
+        public class Geometry
+        {
+            public string type { get; set; }
+            public float[] coordinates { get; set; }
+        }
+
+        public class Properties
+        {
+            public string zip { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+            public string address { get; set; }
+            public string borough { get; set; }
+            public string neighborhood { get; set; }
+            public string county { get; set; }
+        }
+    }
+}
